d578e3c2183e577e2db2e2077447034ecb5fc479
Olivia Nordquist <nolivia@umich.edu>
2015-04-05 18:00:44 -0400

    oops misread intsr

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ class Card:
         self.symbol = set_symbol()
         self.shading = set_shading()
         self.number = len(decide[1])
+        self.output = decide
 
     def set_shading():
         if ord(decide[1][0]) < 65:
@@ -34,6 +35,7 @@ def readCards():
         data = raw_input().split(" ")
         card = Card(data)
         deck.append(card)
+    return n
 
 def is_valid_set(cards):
     for prop in ['number', 'symbol', 'shade', 'color']:
@@ -41,3 +43,17 @@ def is_valid_set(cards):
         if len(uniques) == 2:
             return False
     return True
+
+if __name__ == "__main__":
+    n = readCards()
+    count_total = 0
+    disjoint_total = 0
+
+    for i in xrange(n-2):
+        for j in xrange(1, n-1):
+            for k in xrange(2, n):
+                if is_valid_set(deck[i], deck[j], deck[k]):
+                    count_total += 1
+
+    print count_total
+    print disjoint_total
